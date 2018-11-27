8de3847d845a57ab61e4a39e24e692a1ea9cab42
hyyong <45348177+hyyong@users.noreply.github.com>
2018-11-27 11:41:40 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 //For command line git yong
-//GitHub modifiaction
-
+//First: GitHub modification
+//Second: Local repository modification
+  
 print("hello.hoyeon")
 print("Tell Your world")
 print("Tell his world")
